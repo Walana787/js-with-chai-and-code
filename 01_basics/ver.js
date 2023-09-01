bd24fb6bd7e2af05a7b90e1a82acd527050c7485
Walana787 <143738682+Walana787@users.noreply.github.com>
2023-09-01 08:16:37 +0000

    veriables and datatype

---
+++ 01_basics/ver.js
@@ -0,0 +1,11 @@
+const accountID = 23489689
+var accountname= "shoaib"
+let accountemail= "shoaibwalana@787"
+accountcity="Rajanpur"
+let accountState="Dg khan"
+// accountid=78787
+console.log(accountID)
+// accountemail="haseebwalana@787"
+// accountname="Walana"
+console.table([accountemail,accountname,accountcity,accountState])
+// /*perfer not to use ver because of issue in block scope or functional sco
